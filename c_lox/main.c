525e871357e8fc674efde1322b12716772710cb7
Ian Saultz <52051793+atierian@users.noreply.github.com>
2022-08-11 21:18:58 -0400

    Initial Commit

---
+++ c_lox/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c_lox
+//
+//  Created by Ian Saultz on 8/11/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
