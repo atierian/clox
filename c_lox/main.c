63cdcefaceace28967578baca7214a081e24a8f5
Ian Saultz <52051793+atierian@users.noreply.github.com>
2022-08-13 12:17:17 -0400

    add bool and nil literals

--- c_lox/main.c
+++ c_lox/main.c
@@ -58,7 +58,7 @@ static void runFile(const char* path) {
     free(source);
     
     if (result == INTERPRET_COMPILE_ERROR) exit(65);
-    if (result == INTERPET_RUNTIME_ERROR) exit(70);
+    if (result == INTERPRET_RUNTIME_ERROR) exit(70);
 }
 
 int main(int argc, const char* argv[]) {
--- c_lox/vm.h
+++ c_lox/vm.h
@@ -16,7 +16,7 @@ typedef struct {
 typedef enum {
 	INTERPRET_OK,
 	INTERPRET_COMPILE_ERROR,
-	INTERPET_RUNTIME_ERROR
+	INTERPRET_RUNTIME_ERROR
 } InterpretResult;
 
 void initVM(void);
