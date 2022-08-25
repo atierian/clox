9737afccdb88b458a2ef6fa4e74448e54e538813
Ian Saultz <52051793+atierian@users.noreply.github.com>
2022-08-25 17:04:03 -0400

    add if/else support

--- c_lox/compiler.c
+++ c_lox/compiler.c
@@ -128,6 +128,16 @@ static void emitBytes(uint8_t byte1, uint8_t byte2) {
     emitByte(byte2);
 }
 
+
+/// Backpatch `then` block from `if` statement.
+/// @param instruction instruction.
+static int emitJump(uint8_t instruction) {
+    emitByte(instruction);
+    emitByte(0xff);
+    emitByte(0xff);
+    return currentChunk()->count - 2;
+}
+
 static void emitReturn(void) {
     emitByte(OP_RETURN);
 }
@@ -227,6 +237,9 @@ static bool identifiersEqual(Token* a, Token* b) {
     return memcmp(a->start, b->start, a->length) == 0;
 }
 
+/// Resolve a local variable.
+/// @param compiler compiler
+/// @param name token name
 static int resolveLocal(Compiler* compiler, Token* name) {
     for (int i = compiler->localCount - 1; i >= 0; i--) {
         Local* local = &compiler->locals[i];
@@ -318,6 +331,38 @@ static void expressionStatement(void) {
     emitByte(OP_POP);
 }
 
+static void patchJump(int offset) {
+    // -2 to adjust for the bytecode for the jump offset itself.
+    int jump = currentChunk()->count - offset - 2;
+    
+    if (jump > UINT16_MAX) {
+        // FIXME: remove arbitrary upper bound limit.
+        // Although you `then` block
+        error("Too much code to jump over.");
+    }
+    
+    currentChunk()->code[offset] = (jump >> 8) & 0xff;
+    currentChunk()->code[offset + 1] = jump & 0xff;
+}
+
+static void ifStatement() {
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'if'.");
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after condition.");
+    
+    int thenJump = emitJump(OP_JUMP_IF_FALSE);
+    emitByte(OP_POP);
+    statement();
+    
+    int elseJump = emitJump(OP_JUMP);
+    
+    patchJump(thenJump);
+    emitByte(OP_POP);
+    
+    if (match(TOKEN_ELSE)) statement();
+    patchJump(elseJump);
+}
+
 static void printStatement(void) {
     expression();
     consume(TOKEN_SEMICOLON, "Expect ';' after value.");
@@ -358,6 +403,8 @@ static void declaration(void) {
 static void statement(void) {
     if (match(TOKEN_PRINT)) {
         printStatement();
+    } else if (match(TOKEN_IF)) {
+        ifStatement();
     } else if (match(TOKEN_LEFT_BRACE)) {
         beginScope();
         block();
