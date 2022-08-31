53235f2d379973f49e62a34e0f1f5f8f1c00e40b
Ian Saultz <52051793+atierian@users.noreply.github.com>
2022-08-31 06:25:10 -0400

    add method call support

--- c_lox/chunk.h
+++ c_lox/chunk.h
@@ -17,6 +17,8 @@ typedef enum {
 	OP_SET_GLOBAL,
 	OP_GET_UPVALUE,
 	OP_SET_UPVALUE,
+	OP_GET_PROPERTY,
+	OP_SET_PROPERTY,
 	OP_EQUAL,
 	OP_GREATER,
 	OP_LESS,
@@ -33,7 +35,9 @@ typedef enum {
 	OP_CALL,
 	OP_CLOSURE,
 	OP_CLOSE_UPVALUE,
-	OP_RETURN
+	OP_RETURN,
+	OP_CLASS,
+	OP_METHOD
 } OpCode;
 
 typedef struct {
